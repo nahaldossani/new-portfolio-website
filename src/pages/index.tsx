6d7c9724ec90153f4b9f1962b2af1c1c92c12ea1
Nahal Dossani <101011553+nahaldossani@users.noreply.github.com>
2023-03-24 12:34:52 +0000

    commit -- 24/3/23

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,12 +15,8 @@ export default index
 
 export const getServerSideProps = async (context: GetServerSidePropsContext) => {
   //calculation
-  const res = await fetch('https://nahaldossani-didactic-fiesta-wqxppvx95wqcq67-3000.preview.app.github.dev/api/services')
-  const data = await res.json()
   
   return {
-    props: {
-      services: data.services,
-    }
+    props: {}
   }
 }
