4aa3f7bf7cb5526349f908f562d709ae33782eb1
Nahal Dossani <101011553+nahaldossani@users.noreply.github.com>
2023-05-29 15:08:52 +0000

    hlo

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,7 +1,75 @@
-const projects = () => {
-  return (
-    <div className="p-4">projects page</div>
-  )
-}
+import { useState } from 'react';
 
-export default projects
+const Form = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    // Perform form submission logic here
+    console.log('Name:', name);
+    console.log('Email:', email);
+    console.log('Phone:', phone);
+
+    // Reset form fields
+    setName('');
+    setEmail('');
+    setPhone('');
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="max-w-md mx-auto">
+      <div className="mb-4">
+        <label htmlFor="name" className="block mb-1 font-medium">
+          Name
+        </label>
+        <input
+          type="text"
+          id="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          className="w-full px-4 py-2 border border-gray-300 rounded focus:outline-none focus:ring focus:border-blue-500"
+          required
+        />
+      </div>
+      <div className="mb-4">
+        <label htmlFor="email" className="block mb-1 font-medium">
+          Email
+        </label>
+        <input
+          type="email"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          className="w-full px-4 py-2 border border-gray-300 rounded focus:outline-none focus:ring focus:border-blue-500"
+          required
+        />
+      </div>
+      <div className="mb-4">
+        <label htmlFor="phone" className="block mb-1 font-medium">
+          Phone
+        </label>
+        <input
+          type="tel"
+          id="phone"
+          value={phone}
+          onChange={(e) => setPhone(e.target.value)}
+          className="w-full px-4 py-2 border border-gray-300 rounded focus:outline-none focus:ring focus:border-blue-500"
+          required
+        />
+      </div>
+      <div>
+        <button
+          type="submit"
+          className="px-4 py-2 text-white bg-blue-500 rounded hover:bg-blue-600"
+        >
+          Submit
+        </button>
+      </div>
+    </form>
+  );
+};
+
+export default Form;
